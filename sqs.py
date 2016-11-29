8fd14937d7587be27c8ebdbf3e651c6b5770043a
Karl Gutwin <karl.gutwin@biogen.com>
2016-11-28 19:25:52 -0800

    new redis host

--- sqs.py
+++ sqs.py
@@ -5,13 +5,13 @@ import json
 
 sqs = boto3.resource('sqs')
 
-queue = sqs.get_queue_by_name(QueueName='ubercorn')
+queue = sqs.get_queue_by_name(QueueName='ubercorn-v2')
 
 while True:
     for message in queue.receive_messages():
         if 'donotreplytothis' in message.body:
             message.delete()
             continue
-        r = requests.post('http://UberCorn-ELB-503246632.eu-central-1.elb.amazonaws.com/', data=message.body, headers={'content-type': 'application/json'})
+        r = requests.post('http://ubercorn-v2-1123155696.eu-central-1.elb.amazonaws.com/', data=message.body, headers={'content-type': 'application/json'})
         print message.body, r.status_code
         message.delete()
