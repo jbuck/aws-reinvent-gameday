f5c1413da2f9bfe6eab357f8893bfd7a2635bd14
Jon Buckley <jbuckley@mozilla.com>
2016-11-28 19:52:55 -0500

    Add redis to node client

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var redis = require("redis");
+var client = redis.createClient(process.env.REDIS_URL);
 var request = require('request');
 var express = require('express');
 var app = express();
@@ -10,6 +12,7 @@ var MESSAGES_LENGTH = {};
 var RECIEVED = 0;
 var SENT = 0;
 
+app.use(require('morgan')('tiny'));
 app.use(require('body-parser').json());
 
 app.get('/', function (req, res) {
@@ -26,41 +29,45 @@ app.get('/health', (req, res) => {
 });
 
 app.post('/', function (req, res) {
-  RECIEVED++;
+  var msg_data = req.body.Data,
+      msg_id = req.body.Id,
+      msg_part_number = req.body.PartNumber,
+      msg_total = req.body.TotalParts,
+      redis_key = msg_id + ".data"
 
-  if (!MESSAGES[req.body.Id]) {
-    MESSAGES[req.body.Id] = [];
-    MESSAGES_LENGTH[req.body.Id] = req.body.TotalParts;
-  }
+  var time_for_redis = Date.now();
 
-  MESSAGES[req.body.Id][req.body.PartNumber] = req.body.Data;
+  client.multi()
+    .zadd([redis_key, msg_part_number, msg_data])
+    .zrange([redis_key, 0, msg_total])
+    .expire([redis_key, 600])
+    .exec((multi_error, replies) => {
+      console.log("REDIS %s %d ms", redis_key, (Date.now() - time_for_redis));
 
-  console.log(req.body.Id + " = " + MESSAGES[req.body.Id]);
-
-  var ready_to_send = true;
-  for (var i = 0; i < MESSAGES_LENGTH[req.body.Id]; i++) {
-    ready_to_send = ready_to_send && !!MESSAGES[req.body.Id][i];
-  }
-
-  if (ready_to_send) {
-    SENT++;
-
-    var return_data = MESSAGES[req.body.Id].join("");
-    request({
-      method: "POST",
-      uri: api_base + "/" + req.body.Id,
-      body: return_data,
-      headers: {
-        'x-gameday-token': api_token
-      }
-    }, (err, resp, body) => {
-      if (err || resp.statusCode != 200) {
-        console.log(err);
+      if (multi_error) {
+        return console.log("Redis error: " + multi_error);
       }
 
-      console.log(api_base + "/" + req.body.Id + " with " + return_data + " returned " + resp.statusCode);
+      var returned_data = replies[1];
+      var time_for_request = Date.now();
+
+      if (returned_data.length === msg_total) {
+        request({
+          method: "POST",
+          uri: api_base + "/" + msg_id,
+          body: returned_data.join(""),
+          headers: {
+            'x-gameday-token': api_token
+          }
+        }, (err, resp, body) => {
+          if (err || resp.statusCode != 200) {
+            console.log(err);
+          }
+
+          console.log("POST " + api_base + "/" + req.body.Id + " " + resp.statusCode + " " + returned_data.join("") + " " + (Date.now() - time_for_request) + " ms");
+        });
+      }
     });
-  }
 
   res.sendStatus(200);
 });
